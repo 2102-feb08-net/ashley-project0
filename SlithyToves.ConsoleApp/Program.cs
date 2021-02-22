7fee84b233da07eba0b4cf74afb3c55834453e68
Ashley Brown <1thing.ashleyb@gmail.com>
2021-02-22 09:00:28 -0600

    fleshed out main menu

--- SlithyToves.ConsoleApp/Program.cs
+++ SlithyToves.ConsoleApp/Program.cs
@@ -24,36 +24,116 @@ namespace SlithyToves.ConsoleApp
             while (true) 
             {
                 PrintMainMenu();
-                if (GetMenuSelection == 1)
+                if (GetMenuSelection().Equals(1))
                 {
-                    Console.WriteLine("Enter a customer ID: ");
-                    var id = Convert.ToInt32(Console.ReadLine());
-
+                    //GetCustomerByLastName(lastName);
+                    PrintShortMenu();
+                    if (GetMenuSelection().Equals(1))
+                    {
+                        continue;
+                    }
+                    else 
+                    {
+                        break;
+                    }
                 }
+                if (GetMenuSelection().Equals(2)) 
+                {
+                    //CreateCustomer();
+                    PrintShortMenu();
+                    if (GetMenuSelection().Equals(1))
+                    {
+                        continue;
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                } 
+                if (GetMenuSelection().Equals(3)) 
+                {
+                    //CreateOrder();
+                    PrintShortMenu();
+                    if (GetMenuSelection().Equals(1))
+                    {
+                        continue;
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                } 
+                if (GetMenuSelection().Equals(4)) 
+                {
+                    //GetOrdersByCustomer();
+                    PrintShortMenu();
+                    if (GetMenuSelection().Equals(1))
+                    {
+                        continue;
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                } 
+                if (GetMenuSelection().Equals(5)) 
+                {
+                    //GetOrderDetailsById();
+                    PrintShortMenu();
+                    if (GetMenuSelection().Equals(1))
+                    {
+                        continue;
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                } 
+                if (GetMenuSelection().Equals(6)) 
+                {
+                    //GetOrdersByLocation();
+                    PrintShortMenu();
+                    if (GetMenuSelection().Equals(1))
+                    {
+                        continue;
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                } 
+                if (GetMenuSelection().Equals(7)) 
+                {
+                    Console.Write("Have a nice day!");
+                    break;
+                } 
             }
+            
         }
 
         public static void PrintMainMenu() 
         {
             Console.WriteLine("\nSlithy Toves Bookish Interface\n");
             Console.WriteLine("Main Menu\n");
-            Console.WriteLine("1 - To view a list of current customers");
+            Console.WriteLine("1 - To search for customers by last name");
             Console.WriteLine("2 - To add a customer");
             Console.WriteLine("3 - To place an order");
             Console.WriteLine("4 - To view the order history of a customer");
             Console.WriteLine("5 - To view the details of an order");
             Console.WriteLine("6 - To view the order history of a location");
             Console.WriteLine("7 - To quit\n");
-            Console.WriteLine("Select an option 1-7: ");
+            Console.Write("Select an option 1-7: ");
         }
 
         public static void PrintShortMenu() 
         {
-            Console.WriteLine("To return to the main menu, please enter \"1\"");
+            Console.WriteLine("\nTo return to the main menu, please enter \"1\"");
             Console.WriteLine("To quit, enter \"2\"");
         }
 
         public static int GetMenuSelection() => Convert.ToInt32(Console.ReadLine());
+
+        
         
     }
 }
